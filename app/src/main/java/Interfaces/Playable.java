48ef7c1edeebc23b4abead54266241c3b1bc977c
Kynan <k.song@blueyonder.co.uk>
2017-11-03 11:39:55 +0000

    Created Playable and Sellable interfaces.

---
+++ app/src/main/java/Interfaces/Playable.java
@@ -0,0 +1,9 @@
+package Interfaces;
+
+/**
+ * Created by kynansong on 03/11/2017.
+ */
+
+public interface Playable {
+    String play();
+}
---
+++ app/src/main/java/Interfaces/Sellable.java
@@ -0,0 +1,9 @@
+package Interfaces;
+
+/**
+ * Created by kynansong on 03/11/2017.
+ */
+
+public interface Sellable {
+    double markup(int sellprice, int buyprice);
+}
